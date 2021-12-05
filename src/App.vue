eb750f44e8bc91043a63de36e9573f7eb2902cb2
JankTI <1308315453@qq.com>
2021-12-05 21:04:36 +0800

    项目初始化-调整初始目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <h1>热门头条</h1>
+    <router-view></router-view>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
 
-#nav {
-  padding: 30px;
+    };
+  },
+};
+</script>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+<style lang="less" scoped>
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
